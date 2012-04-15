8781e2b9ed16b5e03a5542c2e8f61157307583d3
jerome creignou <jerome.creignou@gmail.com>
2012-04-15 16:09:09 +0200

    enable read from stdin.

--- extractor/lib/Extractor.js
+++ extractor/lib/Extractor.js
@@ -2,9 +2,10 @@ var fs = require('fs'), util = require('util');
 var EventEmitter = require('events').EventEmitter;
 var carrier = require('carrier');
 
-function Extractor(options) {
+function Extractor(ac) {
     EventEmitter.call(this);
     this.matchers = [];
+    this.vars = ac || {};
 }
 util.inherits(Extractor, EventEmitter);
 module.exports = Extractor;
@@ -16,13 +17,17 @@ Extractor.prototype.matches = function(regexp,cb){
 
 Extractor.prototype.start = function(stream){
     var self = this;
+    if(!stream){
+        stream = process.stdin;
+        stream.resume();
+    }
     var car = carrier.carry(stream);
     car.on('line',  function(line) {
         for(i=0;i<self.matchers.length;i++){
             var matcher = self.matchers[i];
             var m = matcher.re.exec(line);
             if(m && matcher.callback){
-                matcher.callback(m);
+                matcher.callback(m, self.vars);
                 break;
             }
         }
--- extractor/test/test-Extractor.js
+++ extractor/test/test-Extractor.js
@@ -1,10 +1,20 @@
 var vows = require('vows'),
     assert = require('assert'),
-    Extractor = require('../lib1/Extractor');
+    Extractor = require('../lib/Extractor');
     
 vows.describe('Extractor').addBatch({
     'Extractor object : ':{
         topic: new Extractor(),
+        'as matches method :' : function(e){
+            assert.ok(e.matches);
+        },
+        'as start method :' : function(e){
+            assert.ok(e.start);
+        }
+    }
+}).addBatch({
+    'Extractor object with accumulator : ':{
+        topic: new Extractor({myvar:0}),
         'as matches method :' : function(e){
             assert.ok(e.matches);
         },
