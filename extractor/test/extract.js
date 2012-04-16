a4d6a2c5c648ec30c63919bb8d0dce3b4c45acd6
jerome creignou <jerome.creignou@gmail.com>
2012-04-16 23:33:24 +0200

    complete exemple

--- extractor/test/extract.js
+++ extractor/test/extract.js
@@ -2,6 +2,14 @@ var util = require('util'),
     extractor = require('../lib/main.js'),
     fs = require('fs');
 
-//var s = fs.createReadStream('./sample.csv',{});
+var s = fs.createReadStream(__dirname + '/sample.csv',{});
+var ac = {};
+ac.count = 0;
+extractor(ac).matches(/;(?!(?:[^",]|[^"],[^"])+")/,function(m,vars){
+    console.log(m);
+    vars.count ++;
+}).on('end',function(){
+    console.log(ac.count + ' matches found.');
+}).start(s);
 
-extractor().matches(/;(?!(?:[^",]|[^"],[^"])+")/,function(m){console.log(m)}).start();
+
