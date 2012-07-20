3e36b86948cd8e4a5f90676c0c51e374e494aec8
jerome creignou <jerome.creignou@gmail.com>
2012-07-20 12:52:08 +0200

    use 'request' module

--- json-rpc/client.js
+++ json-rpc/client.js
@@ -1,9 +1,16 @@
-var Client = require('./json-rpc'), util = require('util');
+var jsonrpc = require('./json-rpc'), util = require('util');
 
 //var cli = new Client('wse1162.maafprod.e-corail.com','/trac/framework/jsonrpc');
-var cli = Client('www.freenet.org.nz','/dojo/pyjson/jsonrpctest.cgi');
+var cli = jsonrpc('http://xp-dev.com/trac/devianne/rpc');
 
-cli.call("pow",[2,3],function(err,data){
-    console.log(data);
+cli.callRpc('system.getAPIVersion',[],function(err,res,body){
+    if(err){
+        console.log(err);
+    }
+    console.log(body);
 });
+cli.on('ready',function(p){
+    console.log(util.inspect(p));
+});
+cli.proxy();
 
--- json-rpc/json-rpc.js
+++ json-rpc/json-rpc.js
@@ -1,79 +1,54 @@
-var http = require('http'), util = require('util'), events = require('events');
-function Client (host, path, port){
+var http = require('http'), util = require('util'), EventEmitter = require('events').EventEmitter,request = require('request');
+function jsonrpc (url){
+    EventEmitter.call(this);
 	var self = this;
-	self.server = {'port': port||80, 'host':host, 'path':path||'/'},
-		
-	self.call = function(method,params,callback){
-		
-		httpResult(self.server,{'method':method,'params':params,'id':new Date().getTime()},callback);
-	}
-	
-	self.proxy = function(){
-		self.call('system.listMethods',[], function (err, data){
-				if(err || !data || !data.result){
-					console.log('unable to build proxy '+err);
-                    self.emit('error', err);
-					return;
-				}
-				data.result.forEach(function (method){
-					var fn = (function(method){ return function (){
-						var params = Array.prototype.slice.call(arguments),
-							cb = params.length && typeof params[params.length-1] =="function" && params.pop();
-						return self.call(method,params,function(err,data){ 
-							if(data){
-								cb(data.err,data.result);
-							}else{
-								cb(err,null);
-							}
-						});
-					}})(method);
-					if(method.indexOf('.') == -1){
-						self[method] = fn;
-					}else{
-						curr = self;
-						var parts = method.split('.');
-						for(i =0 ;i<parts.length-1;i++){
-							var part = parts[i];
-							if(!curr[part]){
-								curr[part] = {};
-							}
-							curr = curr[part];
-						}
-						curr[parts.pop()]=fn;
-					}
-				});
-				self.emit('ready', self);
-			}
-		);
-	}
-	self.proxy();
-
-	
-	function httpResult(server,callinfo,responseHandler){
-		var client = http.createClient(server.port||80, server.host);
-		//console.log('url : ' + util.inspect(server));
-		var headers = {'host':server.host,'Content-Type':'application/json'};
-		var params = JSON.stringify(callinfo);
-		headers["Content-Length"] = params.length;
-		var request = client.request('POST', server.path || '/', headers);
-		//console.log(params);
-		request.write(params);
-		request.on('response', function (response) {
-		  //console.log('STATUS: ' + response.statusCode);
-		  if(response.statusCode != 200){
-			responseHandler({'error':response.statusCode},null);
-		  }	
-		  response.setEncoding('utf8');
-		  var data = '';
-		  response.on('data', function (chunk) {
-			data +=chunk;
-		  });
-		  response.on('end', function () {
-			responseHandler(null,JSON.parse(data));
-		  });
-		});
-		request.end();
-	}
+    self.request = request.defaults({'url' : url, json:true});
 }
-util.inherits(Client, events.EventEmitter);
-module.exports = Client;
+util.inherits(jsonrpc, EventEmitter);
+module.exports = function(u){
+    return new jsonrpc(u);
+};;
+
+jsonrpc.prototype.callRpc = function(method,params,callback){		
+	this.request.post({body:JSON.stringify({'method':method,'params':params,'id':new Date().getTime()})},callback);
+};
+
+jsonrpc.prototype.proxy = function(){
+    var self = this;
+	self.callRpc('system.listMethods',[], function (err, res, data){
+			if(err || !data || !data.result){
+				console.log('unable to build proxy '+err);
+                self.emit('error', err);
+				return;
+			}
+			data.result.forEach(function (method){
+				var fn = (function(method){ return function (){
+					var params = Array.prototype.slice.call(arguments),
+						cb = params.length && typeof params[params.length-1] =="function" && params.pop();
+					return self.call(method,params,function(err,data){ 
+						if(data){
+							cb(data.err,data.result);
+						}else{
+							cb(err,null);
+						}
+					});
+				}})(method);
+				if(method.indexOf('.') == -1){
+					self[method] = fn;
+				}else{
+					curr = self;
+					var parts = method.split('.');
+					for(i =0 ;i<parts.length-1;i++){
+						var part = parts[i];
+						if(!curr[part]){
+							curr[part] = {};
+						}
+						curr = curr[part];
+					}
+					curr[parts.pop()]=fn;
+				}
+			});
+			self.emit('ready', self);
+		}
+	);
+};
